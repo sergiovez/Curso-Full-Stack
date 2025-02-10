64917520cd6697abb0693791b4e41e33a6396011
root <root@LAPTOP-UEJ5TKMC>
2025-02-10 12:16:39 +0100

    pre-proyectos CSS

--- Sergio/Frontend/CSS/Ejercicios/Frameworks/vite-project/tailwind.config.js
+++ Sergio/Frontend/CSS/Ejercicios/Frameworks/vite-project/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["index.html"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./public/**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
